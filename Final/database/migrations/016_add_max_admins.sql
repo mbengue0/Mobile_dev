c3d46e768d23413e4b2b4d5d96525b3469abbb50
mbengue0 <mouhamb20@gmail.com>
2026-01-31 14:41:20 +0000

    Final folder deleted

--- Final/database/migrations/016_add_max_admins.sql
+++
@@ -1,6 +0,0 @@
--- Add max_admins to institutions
-ALTER TABLE public.institutions 
-ADD COLUMN max_admins INTEGER DEFAULT 2 NOT NULL;
-
--- Notify PostgREST to reload schema
-NOTIFY pgrst, 'reload';
